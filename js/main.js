9c9f38a3852643204b69ce46b474f21ea1f6319f
pdsuwwz <pdsu.wwz@foxmail.com>
2018-04-16 15:06:42 +0800

    update main.js

--- js/main.js
+++ js/main.js
@@ -8,23 +8,38 @@
  * clickMenu
  */
 (function() {
+  var menuBtn = document.querySelector('#headerMenu')
+  var nav = document.querySelector('#headerNav')
   if (window.innerWidth <= 770) {
-    var menuBtn = document.querySelector('#headerMenu')
-    var nav = document.querySelector('#headerNav')
     menuBtn.onclick = function(e) {
-      e.stopPropagation()
+      e.stopPropagation();
       if (menuBtn.classList.contains('active')) {
-        menuBtn.classList.remove('active')
-        nav.classList.remove('nav-show')
+        menuBtn.classList.remove('active');
+        nav.classList.remove('nav-show');
       } else {
-        nav.classList.add('nav-show')
-        menuBtn.classList.add('active')
+        nav.classList.add('nav-show');
+        menuBtn.classList.add('active');
       }
     }
     document.querySelector('body').addEventListener('click', function() {
-      nav.classList.remove('nav-show')
-      menuBtn.classList.remove('active')
-    })
+      nav.classList.remove('nav-show');
+      menuBtn.classList.remove('active');
+    });
+  }else{
+    menuBtn.onclick = function(e) {
+      e.stopPropagation();
+      if (menuBtn.classList.contains('active')) {
+        menuBtn.classList.remove('active');
+        nav.classList.remove('nav-show');
+      } else {
+        nav.classList.add('nav-show');
+        menuBtn.classList.add('active');
+      }
+    }
+    document.querySelector('body').addEventListener('click', function() {
+      nav.classList.remove('nav-show');
+      menuBtn.classList.remove('active');
+    });
   }
 }());
 

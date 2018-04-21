83ae610b0925f1cfba1b17a45ccae47f288ada46
pdsuwwz <pdsu.wwz@foxmail.com>
2018-04-21 23:03:48 +0800

    update touch

--- js/loading.js
+++ js/loading.js
@@ -2,9 +2,15 @@
  * author: pdsuwwz
  * 
  */
+var $ = function(selector){
+    return document.querySelector(selector);
+};
 window.addEventListener("load", function() {
 	var wrapper = document.getElementById("wrapper");
 	setTimeout(function() {
 		document.body.removeChild(wrapper);
+
+		$('html').setAttribute("style","overflow:auto;");
+		$('body').setAttribute("style","overflow:auto;");
 	}, 1200);
 });

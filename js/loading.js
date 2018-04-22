a8bbbdea05d442dc947f12f810aa4fbb22006eae
pdsuwwz <pdsu.wwz@foxmail.com>
2018-04-22 10:37:32 +0800

    add laoding.js

--- js/loading.js
+++ js/loading.js
@@ -11,9 +11,14 @@
 document.addEventListener("touchstart",move,false);
 window.addEventListener("load", function() {
 	var wrapper = document.getElementById("wrapper");
+	document.body.style.overflow="hidden";
 	setTimeout(function() {
 		document.removeEventListener("touchstart",move,false);
 		document.body.removeChild(wrapper);
 	}, 1200);
+	
+	setTimeout(function(){
+        document.body.style.overflowY="scroll";
+    },1100);
 });
 
